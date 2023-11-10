dc08b48fd91d1879ee30cf7a0be70d7798263abe
jeff26-ux <150452481+jeff26-ux@users.noreply.github.com>
2023-11-10 21:17:05 +0700

    Update index.py

--- index.py
+++ index.py
@@ -1,14 +1,26 @@
-import openai
+import openai 
+import streamlit as st
+with st.sidebar:
+    openai_api_key = st.text_input("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", key="chatbot_api_key", type="password")
+    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
+    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
-# Set your OpenAI API key
-openai.api_key = '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'
+st.title("💬 Chatbot") if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
-# Make a request without specifying the model
-response = openai.Completion.create(
-  engine="text-davinci-003",  # Specify the engine if needed
-  prompt="Kucing"
-)
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
 
-# Access the generated text
-generated_text = response['choices'][0]['text']
-print(generated_text)
+if prompt := st.chat_input():
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
+
+    openai.api_key = openai_api_key
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    msg = response.choices[0].message
+    st.session_state.messages.append(msg)
+    st.chat_message("assistant").write(msg.content)
